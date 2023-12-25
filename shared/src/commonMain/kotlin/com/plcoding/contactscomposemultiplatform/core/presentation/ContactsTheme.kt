68b2bf361933f17486ee851a321ad0708d4d9940
Ryan Wong <ryan@rwmobi.com>
2023-12-26 02:35:26 +0800

    [chore] ContactsTheme: add back missing annotation

--- shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/core/presentation/ContactsTheme.kt
+++ shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/core/presentation/ContactsTheme.kt
@@ -2,6 +2,7 @@ package com.plcoding.contactscomposemultiplatform.core.presentation
 
 import androidx.compose.runtime.Composable
 
+@Composable
 expect fun ContactsTheme(
     darkTheme: Boolean,
     dynamicColor: Boolean,
