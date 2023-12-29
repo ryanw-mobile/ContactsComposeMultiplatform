62a9f3cf9ecfb2edec7994d2910a578e030d8525
Ryan Wong <ryan@rwmobi.com>
2023-12-29 21:06:03 +0800

    [chore] gradle: hoping to fix ios build but doesn't work :(   <-- Greg like putting this in commits

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -30,7 +30,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(compose.runtime)
+                api(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material3)
                 implementation(compose.materialIconsExtended)
