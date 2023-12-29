fc412bffac416aeb5efa8bb0ddd477c29aad6eb9
Ryan Wong <ryan@rwmobi.com>
2023-12-29 22:38:17 +0800

    [feature] App: implement ContactDetailSheet - now edit and delete also work

--- shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/contacts/presentation/ContactListScreen.kt
+++ shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/contacts/presentation/ContactListScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.plcoding.contactscomposemultiplatform.contacts.domain.Contact
 import com.plcoding.contactscomposemultiplatform.contacts.presentation.components.AddContactSheet
+import com.plcoding.contactscomposemultiplatform.contacts.presentation.components.ContactDetailSheet
 import com.plcoding.contactscomposemultiplatform.contacts.presentation.components.ContactListItem
 import com.plcoding.contactscomposemultiplatform.core.presentation.ImagePicker
 
@@ -80,6 +81,12 @@ fun ContactListScreen(
             }
         }
     }
+
+    ContactDetailSheet(
+        isOpen = state.isSelectedContactSheetOpen,
+        selectedContact = state.selectedContact,
+        onEvent = onEvent,
+    )
 
     AddContactSheet(
         state = state,
